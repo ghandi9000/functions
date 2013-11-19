793e36b23e9ed6b66c72c5e9c98ae74b9f391566
Noah Peart <noah.v.peart@gmail.com>
2013-11-18 21:44:00 -0500

    Batch update

---
+++ functions-plotting.R
@@ -0,0 +1,15 @@
+## Functions to help with creating graphics
+
+# panel.smooth function is built in.
+# panel.cor puts correlation in upper panels, size proportional to correlation
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor, ...)
+{
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(0, 1, 0, 1))
+    r <- abs(cor(x, y))
+    txt <- format(c(r, 0.123456789), digits=digits)[1]
+    txt <- paste(prefix, txt, sep="")
+    if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+    text(0.5, 0.5, txt, cex = cex.cor * r)
+}
+
